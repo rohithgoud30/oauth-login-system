536747e359f7ceec34e98e7c7c74e7c8f2c51731
Rohith Goud Panjala <rohithgoud30@gmail.com>
2025-07-11 10:26:32 -0400

    Feat(next): add no-referrer policy for images

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,19 @@ const nextConfig = {
 			},
 		],
 	},
+	async headers() {
+		return [
+			{
+				source: "/:path*",
+				headers: [
+					{
+						key: "Referrer-Policy",
+						value: "no-referrer",
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
